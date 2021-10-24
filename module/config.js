346e08c3610843a2b79e51956cb09e805abf0f4f
Draconas <draconas@gmail.com>
2021-10-24 19:53:12 +0100

    Auto set actor to status bloodied, dying or dead based on HP. PC's go bloodied -> dying -> dead NPC's go bloodied -> dead
    Being dead sets the token as defeated in the combat tracker.
    
    Replacement Dead Status Icon
    
    Some refactoring for more readability

---
+++ module/config.js
@@ -0,0 +1,56 @@
+import DnD4eTools from "./4e-tools.js";
+
+export function registerConfigs() {
+    game.settings.register(DnD4eTools.ID, DnD4eTools.SETTINGS.CREATE_IN_ENCOUNTER_FOLDERS, {
+        name: `TOOLS4E.settings.${DnD4eTools.SETTINGS.CREATE_IN_ENCOUNTER_FOLDERS}.Name`,
+        default: true,
+        type: Boolean,
+        scope: 'client',
+        config: true,
+        hint: `TOOLS4E.settings.${DnD4eTools.SETTINGS.CREATE_IN_ENCOUNTER_FOLDERS}.Hint`
+    });
+
+    game.settings.register(DnD4eTools.ID, DnD4eTools.SETTINGS.DO_NOT_DUPLICATE, {
+        name: `TOOLS4E.settings.${DnD4eTools.SETTINGS.DO_NOT_DUPLICATE}.Name`,
+        default: true,
+        type: Boolean,
+        scope: 'client',
+        config: true,
+        hint: `TOOLS4E.settings.${DnD4eTools.SETTINGS.DO_NOT_DUPLICATE}.Hint`
+    });
+
+    game.settings.register(DnD4eTools.ID, DnD4eTools.SETTINGS.DO_NOT_DUPLICATE_IN_FOLDER, {
+        name: `TOOLS4E.settings.${DnD4eTools.SETTINGS.DO_NOT_DUPLICATE_IN_FOLDER}.Name`,
+        default: true,
+        type: Boolean,
+        scope: 'client',
+        config: true,
+        hint: `TOOLS4E.settings.${DnD4eTools.SETTINGS.DO_NOT_DUPLICATE_IN_FOLDER}.Hint`
+    });
+
+    game.settings.register(DnD4eTools.ID, DnD4eTools.SETTINGS.BLOODIED_ICON, {
+        name: `TOOLS4E.settings.${DnD4eTools.SETTINGS.BLOODIED_ICON}.Name`,
+        default: true,
+        type: Boolean,
+        scope: 'client',
+        config: true,
+        hint: `TOOLS4E.settings.${DnD4eTools.SETTINGS.BLOODIED_ICON}.Hint`
+    });
+    game.settings.register(DnD4eTools.ID, DnD4eTools.SETTINGS.DEAD_ICON, {
+        name: `TOOLS4E.settings.${DnD4eTools.SETTINGS.DEAD_ICON}.Name`,
+        default: true,
+        type: Boolean,
+        scope: 'client',
+        config: true,
+        hint: `TOOLS4E.settings.${DnD4eTools.SETTINGS.DEAD_ICON}.Hint`
+    });
+    game.settings.register(DnD4eTools.ID, DnD4eTools.SETTINGS.CHANGE_DEAD_ICON, {
+        name: `TOOLS4E.settings.${DnD4eTools.SETTINGS.CHANGE_DEAD_ICON}.Name`,
+        default: false,
+        type: Boolean,
+        scope: 'client',
+        config: true,
+        hint: `TOOLS4E.settings.${DnD4eTools.SETTINGS.CHANGE_DEAD_ICON}.Hint`,
+        onChange: () => DnD4eTools.setDeadIcon()
+    });
+}
